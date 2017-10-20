a22e79c5f75d2efe2dfa18c5bda08045e8b2dd54
durajjd <32506032+durajjd@users.noreply.github.com>
2017-10-20 23:59:50 +0530

    Add files via upload

---
+++ src/main/java/com/rbs/TMO/Jayadevi/CustomMessage.java
@@ -0,0 +1,13 @@
+package com.rbs.TMO.Jayadevi.util;
+
+public class CustomMessage {
+	 private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
